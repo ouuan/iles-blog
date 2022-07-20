dce6948123a30b19ce58a317cbbcf82a3a330d5d
Yufan You <ouuansteve@gmail.com>
2022-07-20 22:40:56 +0800

    feat(CopyrightNotice): add width and height attrs to license picture

--- src/components/CopyrightNotice.vue
+++ src/components/CopyrightNotice.vue
@@ -42,6 +42,8 @@
           <img
             alt="CC BY-SA 4.0 License Picture"
             src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"
+            width="88"
+            height="31"
           >
         </a>
       </span>
