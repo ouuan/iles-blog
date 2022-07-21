7be8eb8ab2c82b802d5eb179fd9532056f157a1a
Yufan You <ouuansteve@gmail.com>
2022-07-21 13:02:13 +0800

    feat(posts/visitor-desc): display full visitor count update time

--- src/pages/posts/visitor-desc/page/[page].vue
+++ src/pages/posts/visitor-desc/page/[page].vue
@@ -9,7 +9,7 @@
       :page-index="pageIndex"
       href-prefix="/posts/visitor-desc/page"
       title="文章列表: 最多访问"
-      :description="`排名更新于 ${format(new Date(), 'yyyy-MM-dd')}，非实时更新，所以可能与当前实际访问量排名有小的出入`"
+      :description="`数据更新于 ${format(new Date(), 'yyyy-MM-dd HH:mm')}，非实时更新，所以可能与当前实际访问量有一定差别`"
       display-visitor
     />
   </div>
