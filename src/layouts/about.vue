9edc9497769c2a655b67d3246ee7cd0b9e5a5fbf
Yufan You <ouuansteve@gmail.com>
2022-07-22 17:28:49 +0800

    feat: add about

---
+++ src/layouts/about.vue
@@ -0,0 +1,48 @@
+<template layout="base">
+  <div class="m-3 standard-card">
+    <article
+      itemprop="mainEntity"
+      itemscope
+      itemtype="https://schema.org/Article"
+    >
+      <microdata-author />
+      <post-head
+        client:load
+        :href="page.meta.href"
+        :filename="page.meta.filename"
+        :frontmatter="page.frontmatter"
+      />
+      <section
+        class="article-style"
+        itemprop="articleBody"
+      >
+        <slot />
+      </section>
+    </article>
+    <giscus-comments />
+  </div>
+</template>
+
+<script setup lang="ts">
+const page = usePage();
+
+useHead({
+  meta: [
+    { name: 'og:type', content: 'article' },
+    { name: 'article:published_time', content: page.frontmatter.date.toISOString() },
+    { name: 'article:modified_time', content: page.frontmatter.lastUpdated.toISOString() },
+    { name: 'article:author', content: page.site.author },
+  ],
+  style: [{
+    type: 'text/css',
+    children: `
+.muse { color: #E50080 !important; }
+.aqours { color: #19B1F6 !important; }
+.nijigasaki { color: #F8B656 !important; }
+.liella { color: #DA57D8 !important; }
+.kanon { color: #FF7F27 !important; }
+.kuku { color: #A0FFF9 !important; }
+.mia { color: #A9A898 !important; }`,
+  }],
+});
+</script>
