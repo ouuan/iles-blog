007a288885b3b9b1a1bdea7ed88114dafcc992b2
Yufan You <ouuansteve@gmail.com>
2022-08-07 01:38:30 +0800

    feat(TableOfContentsNode): add animation for the appearance of new nodes

--- src/components/TableOfContentsNode.vue
+++ src/components/TableOfContentsNode.vue
@@ -7,15 +7,18 @@
       class="my-1 relative"
     >
       <a
-        :class="['transition-color ease-in', node.current && 'text-link']"
+        :class="['transition-color ease-out', node.current && 'text-link']"
         :href="`#${node.slug}`"
       >
-        <span :class="`absolute top-1.2 left--4 ${icon(node)}`" />
+        <span
+          :class="`absolute top-1.2 left--4
+                   transition-transform ease-out ${icon(node)}`"
+        />
         <span>{{ node.title }}</span>
       </a>
       <div
         v-if="(node.open || showAll) && node.children.length > 0"
-        class="pl-4 b-l-2 b-area"
+        class="animate-appear pl-4 b-l-2 b-area"
       >
         <table-of-contents-node
           :nodes="node.children"
@@ -36,7 +39,22 @@ const props = defineProps<{
 
 function icon(node: TOCNode) {
   if (node.children.length === 0) return 'i-mdi-circle-medium';
-  if (node.open || props.showAll) return 'i-mdi-chevron-down';
+  if (node.open || props.showAll) return 'i-mdi-chevron-right rotate-90';
   return 'i-mdi-chevron-right';
 }
 </script>
+
+<style scoped lang="scss">
+.animate-appear {
+  animation: appear .15s cubic-bezier(0, 0, 0.2, 1);
+}
+
+@keyframes appear {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+</style>
