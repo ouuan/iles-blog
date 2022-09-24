dd643c87517aeba9b23e98c81d5c417305a148a0
Yufan You <ouuansteve@gmail.com>
2022-09-24 12:15:44 +0800

    fix(TextJustifyFix): don't set text align if parent is table-cell

--- src/components/TextJustifyFix.vue
+++ src/components/TextJustifyFix.vue
@@ -8,13 +8,15 @@ export function onLoad() {
   const math = Array.from(document.getElementsByClassName('math-inline'));
   const code = Array.from(document.querySelectorAll(':not(pre) > code'));
   math.concat(code).forEach((node) => {
-    if (node.parentElement) {
-      if (node.getBoundingClientRect().width
-          > node.parentElement.getBoundingClientRect().width / 3) {
-        // eslint-disable-next-line no-param-reassign
-        node.parentElement.style.textAlign = 'initial';
-      }
+    if (!node.parentElement) return;
+    if (getComputedStyle(node.parentElement).display === 'table-cell') {
+      return;
     }
+    if (node.getBoundingClientRect().width < node.parentElement.getBoundingClientRect().width / 3) {
+      return;
+    }
+    // eslint-disable-next-line no-param-reassign
+    node.parentElement.style.textAlign = 'initial';
   });
 }
 </script>
