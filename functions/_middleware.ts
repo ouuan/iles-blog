f9bcf99935c67f7744ee449b7f88504a15cd8237
Yufan You <ouuansteve@gmail.com>
2022-11-20 20:48:46 +0800

    fix(functions): use normal UA for bots and use IP in UA hash

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -5,6 +5,15 @@ import isbot from 'isbot';
 const domain = 'feed.ouuan.moe';
 const plausibleUrl = 'https://plausible.ouuan.moe';
 
+// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
+async function sha1(message: string) {
+  const msgUint8 = new TextEncoder().encode(message);
+  const hashBuffer = await crypto.subtle.digest('SHA-1', msgUint8);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+  return hashHex;
+}
+
 const trackFeeds: PagesFunction = async (context) => {
   const { request } = context;
 
@@ -13,13 +22,15 @@ const trackFeeds: PagesFunction = async (context) => {
   if (/\/(feed|commits)\.(xml|atom|json)\/*$/.test(new URL(request.url).pathname)) {
     const referer = request.headers.get('Referer');
     const ua = request.headers.get('User-Agent') || 'Unknown UA';
+    const ip = request.headers.get('CF-Connecting-IP') || 'Unknown IP';
+    const hash = await sha1(`${ua}-${ip}`);
 
-    function trackEvent(name: string, props?: object) {
+    async function trackEvent(name: string, props?: object) {
       return fetch(`${plausibleUrl}/api/event`, {
         method: 'POST',
         headers: {
-          // I gave up correctly sending the client IP to Plausible
-          'User-Agent': ua,
+          // Plausible requires a normal UA
+          'User-Agent': `${isbot(ua) ? 'Mozilla/5.0 (X11; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0' : ua} (${hash})`,
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
