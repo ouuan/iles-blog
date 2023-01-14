db77c49c542df867f5c220975e25d734b5443166
Yufan You <ouuansteve@gmail.com>
2023-01-14 14:11:21 +0800

    build: only unshallow when shallow

--- build.sh
+++ build.sh
@@ -7,14 +7,16 @@ uname -a
 ldd --version
 
 # get commit log
-git fetch --unshallow
+if [[ $(git rev-parse --is-shallow-repository) == "true" ]]; then
+    git fetch --unshallow
+fi
 
 # unpack objects for https://github.com/rwjblue/git-repo-info/issues/48
 mv .git/objects/pack/pack-*.pack .
 for pack in pack-*.pack; do
     git unpack-objects < "$pack"
 done
-rm pack-*.pack
+rm -f pack-*.pack
 
 # build
 npx pnpm i --store=node_modules/.pnpm-store
