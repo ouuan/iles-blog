491908486dd293ca6067e88ae09bf62022313cde
Yufan You <ouuansteve@gmail.com>
2023-01-15 19:18:03 +0800

    refactor(styles): move character colors to global CSS
    Hope there's no conflict 🙏

--- src/layouts/about.vue
+++ src/layouts/about.vue
@@ -37,19 +37,5 @@ useHead({
     { property: 'profile:username', content: site.author },
     { property: 'profile:gender', content: site.gender },
   ],
-  style: [{
-    type: 'text/css',
-    children: `
-.muse { color: #E50080 !important; }
-.aqours { color: #19B1F6 !important; }
-.nijigasaki { color: #F8B656 !important; }
-.liella { color: #DA57D8 !important; }
-.kanon { color: #FF7F27 !important; }
-.kuku { color: #A0FFF9 !important; }
-.mia { color: #A9A898 !important; }
-.emma { color: #84C36E !important; }
-.honoka { color: #FFAE00 !important; }
-`,
-  }],
 });
 </script>
