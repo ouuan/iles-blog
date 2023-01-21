fa68f7ba8f4066fccdd340eac8fb1614330a9423
Yufan You <ouuansteve@gmail.com>
2023-01-21 18:18:35 +0800

    refactor(styles): set LL colors in unocss instead of characters.scss
    `!important`s are not needed, because the class is set on the <span>
    inside an <a>, not on the <a>.

--- uno.config.ts
+++ uno.config.ts
@@ -31,10 +31,12 @@ export default defineConfig({
       '3xl': '112em',
     },
     colors: {
+      // theme colors
       dark: ['#353853', '#30344C', '#2C2F45', '#282A3E', '#232637', '#1F2130', '#1A1C29', '#161722', '#12131C', '#0D0E15'],
       light: ['#F7F9FB', '#EFF3F7', '#E6EDF2', '#DEE6EE', '#D6E0EA', '#CEDAE6', '#C6D4E2', '#BECEDD', '#B5C8D9', '#ADC2D5'],
       primary: ['#A2C6F0', '#8BB8EC', '#74AAE8', '#5D9CE5', '#468EE1', '#2E80DD', '#2272CF', '#1E66B8', '#1A59A0', '#164C89'],
       gray: ['#F0F0F3', '#E1E2E8', '#D2D3DC', '#C3C5D1', '#B4B6C5', '#A5A7BA', '#9699AE', '#878AA2', '#787B97', '#6A6E8A', '#5F627B', '#53566C', '#474A5D', '#3C3E4E', '#2F313D'],
+      // utility colors
       success: '#3DA83A',
       error: '#B7404C',
       text: 'var(--text-color)',
@@ -48,6 +50,17 @@ export default defineConfig({
       footer: 'var(--footer-color)',
       area: 'var(--area-color)',
       nested: 'var(--nested-color)',
+      // LoveLive! colors
+      muse: '#E50080',
+      honoka: '#FFAE00',
+      aqours: '#19B1F6',
+      nijigasaki: '#F8B656',
+      aisan: '#FF5800',
+      emma: '#84C36E',
+      mia: '#A9A898',
+      liella: '#DA57D8',
+      kanon: '#FF7F27',
+      keke: '#A0FFF9',
     },
   },
   shortcuts: {
