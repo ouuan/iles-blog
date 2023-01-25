bda3b42b8524003bce52dc61621b0e43f554b033
Yufan You <ouuansteve@gmail.com>
2023-01-25 21:56:26 +0800

    feat(PostList): display year separately

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -10,56 +10,65 @@
     <span class="mr-2">共 {{ postCount }} 篇文章</span>
   </div>
   <ol class="my-6 flex flex-col gap-4 b-l-4 b-area">
-    <li
-      v-for="post of posts"
+    <template
+      v-for="(post, index) of posts"
       :key="post.meta.href"
     >
-      <a
-        class="flex items-center gap-3 relative pl-4 group
-               py-1 b-b-2 b-area hover:b-footer transition-colors ease-out b-dotted"
-        :href="post.meta.href"
-        itemscope
-        itemtype="https://schema.org/BlogPosting"
+      <li
+        v-if="sort !== 'visitor-desc' && isPostNewYear(index)"
+        class="flex items-center relative pl-4 mt-2"
       >
-        <span
-          class="absolute left--5px w-6px h-6px rd-3px
+        <span class="absolute left--6px w-8px h-8px rd-4px bg-gray-5 dark:bg-gray-10" />
+        <span class="text-6 font-bold font-serif">{{ format(post.frontmatter.date, 'yyyy') }}</span>
+      </li>
+      <li>
+        <a
+          class="flex items-center gap-3 relative pl-4 group
+               py-1 b-b-2 b-area hover:b-footer transition-colors ease-out b-dotted"
+          :href="post.meta.href"
+          itemscope
+          itemtype="https://schema.org/BlogPosting"
+        >
+          <span
+            class="absolute left--5px w-6px h-6px rd-3px
                  bg-gray-5 dark:bg-gray-10 group-hover:bg-text transition-colors ease-out"
-        />
-        <post-microdata :post="post" />
-        <time
-          :class="[
-            'text-footer shrink-0 tabular-nums',
-            displayVisitor && 'hidden sm:inline-block'
-          ]"
-          :datetime="formatISO(post.frontmatter[displayTime])"
-          :title="`${displayTime === 'date' ? '创建' : '修改'}于 ${
-            format(post.frontmatter[displayTime], 'yyyy-MM-dd HH:mm:ss O')
-          }`"
-          :itemprop="displayTime === 'date' ? 'dateCreated' : 'dateModified'"
-        >
-          <span class="hidden xs:inline">
-            {{ format(post.frontmatter[displayTime], 'yyyy-') }}
+          />
+          <post-microdata :post="post" />
+          <time
+            :class="[
+              'text-footer shrink-0 tabular-nums',
+              displayVisitor && 'hidden sm:inline-block'
+            ]"
+            :datetime="formatISO(post.frontmatter[displayTime])"
+            :title="`${displayTime === 'date' ? '创建' : '修改'}于 ${
+              format(post.frontmatter[displayTime], 'yyyy-MM-dd HH:mm:ss O')
+            }`"
+            :itemprop="displayTime === 'date' ? 'dateCreated' : 'dateModified'"
+          >
+            <span v-if="sort === 'visitor-desc'">
+              {{ format(post.frontmatter[displayTime], 'yyyy-') }}
+            </span>
+            <span>
+              {{ format(post.frontmatter[displayTime], 'MM-dd') }}
+            </span>
+          </time>
+          <span
+            itemprop="headline"
+            class="font-serif text-lg inline-block break-anywhere"
+          >
+            {{ post.frontmatter.title }}
           </span>
-          <span>
-            {{ format(post.frontmatter[displayTime], 'MM-dd') }}
+          <span
+            v-if="displayVisitor"
+            class="ml-auto flex items-center"
+            :title="`访问量: ${post.frontmatter.visitor}`"
+          >
+            <span class="i-mdi-eye-outline mr-1" />
+            <span>{{ post.frontmatter.visitor }}</span>
           </span>
-        </time>
-        <span
-          itemprop="headline"
-          class="font-serif text-lg inline-block break-anywhere"
-        >
-          {{ post.frontmatter.title }}
-        </span>
-        <span
-          v-if="displayVisitor"
-          class="ml-auto flex items-center"
-          :title="`访问量: ${post.frontmatter.visitor}`"
-        >
-          <span class="i-mdi-eye-outline mr-1" />
-          <span>{{ post.frontmatter.visitor }}</span>
-        </span>
-      </a>
-    </li>
+        </a>
+      </li>
+    </template>
   </ol>
   <pagination-bar
     :current-page="pageIndex"
@@ -103,4 +112,10 @@ const propRefs = toRefs(props);
 const posts = usePosts(propRefs);
 const postCount = usePostCount(propRefs.filter);
 const pageCount = usePageCount(propRefs);
+
+function isPostNewYear(index: number) {
+  if (index === 0) return true;
+  return posts.value[index].frontmatter.date.getYear()
+     !== posts.value[index - 1].frontmatter.date.getYear();
+}
 </script>
