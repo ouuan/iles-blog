f3d4ccf6ccf9092c3d410603821929e500f49f09
Yufan You <ouuansteve@gmail.com>
2023-02-03 14:31:17 +0800

    fix(TextJustifyFix): wait for document.fonts.ready

--- src/components/TextJustifyFix.vue
+++ src/components/TextJustifyFix.vue
@@ -2,20 +2,23 @@
   <div />
 </template>
 
-<script client:load lang="ts">
+<script client:idle lang="ts">
 // eslint-disable-next-line import/prefer-default-export
 export function onLoad() {
-  const math = Array.from(document.getElementsByClassName('math-inline'));
-  math.forEach((node) => {
-    if (!node.parentElement) return;
-    if (getComputedStyle(node.parentElement).display === 'table-cell') {
-      return;
-    }
-    if (node.getBoundingClientRect().width < node.parentElement.getBoundingClientRect().width / 3) {
-      return;
-    }
-    // eslint-disable-next-line no-param-reassign
-    node.parentElement.style.textAlign = 'initial';
+  document.fonts.ready.then(() => {
+    const math = Array.from(document.getElementsByClassName('math-inline'));
+    math.forEach((node) => {
+      if (!node.parentElement) return;
+      if (getComputedStyle(node.parentElement).display === 'table-cell') {
+        return;
+      }
+      if (node.getBoundingClientRect().width
+          < node.parentElement.getBoundingClientRect().width / 3) {
+        return;
+      }
+      // eslint-disable-next-line no-param-reassign
+      node.parentElement.style.textAlign = 'initial';
+    });
   });
 }
 </script>
