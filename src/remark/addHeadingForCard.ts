f888198c1cebd201ebac0ac930837edbabd14e49
Yufan You <ouuansteve@gmail.com>
2023-02-05 15:14:38 +0800

    fix(addHeadingForCard): only add explicit titles in TOC & type safe
    1.  Make MDX nodes in mdast type safe.
    2.  Only add Card in TOC if the title is set explicitly.

--- src/remark/addHeadingForCard.ts
+++ src/remark/addHeadingForCard.ts
@@ -5,20 +5,21 @@ import type { Root } from 'mdast';
 
 const addHeadingForCard: Plugin<[], Root> = () => (root) => {
   let hasH2 = false;
-  root.children = root.children.reduce((children, child: any) => {
+  root.children = root.children.reduce((children, child) => {
     if (child.type === 'heading' && child.depth === 2) {
       hasH2 = true;
     } else if (hasH2 && child.type === 'mdxJsxFlowElement' && child.name === 'Card') {
-      children.push({
-        type: 'heading',
-        depth: 6,
-        children: [{
-          type: 'text',
-          value: child.attributes?.find((attr: any) => attr.name === 'title')?.value
-            || child.attributes?.find((attr: any) => attr.name === 'type')?.value
-            || 'Note',
-        }],
-      });
+      const title = child.attributes?.find((attr: any) => attr.name === 'title')?.value;
+      if (typeof title === 'string') {
+        children.push({
+          type: 'heading',
+          depth: 6,
+          children: [{
+            type: 'text',
+            value: title,
+          }],
+        });
+      }
     }
     children.push(child);
     return children;
