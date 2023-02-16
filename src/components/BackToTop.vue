c8cf766f0fa62d65ecc589a98a4fa98c2d451a7d
Yufan You <ouuansteve@gmail.com>
2023-02-16 11:53:27 +0800

    fix(BackToTop): always display to prevent flash of appearance

--- src/components/BackToTop.vue
+++ src/components/BackToTop.vue
@@ -1,46 +1,44 @@
 <template>
-  <template v-if="display">
-    <button
-      v-if="toTop"
-      class="btt-btn floating-button bottom-10"
-      title="回到顶部"
-      aria-label="回到顶部"
-      @click="scrollToTop"
-    >
-      <span class="btt-icon text-xl i-mdi-chevron-double-up" />
-      <span class="btt-percent hidden">
-        <span class="flex justify-center">
-          <span class="text-xs i-mdi-chevron-double-up" />
-        </span>
-        <span
-          class="text-sm"
-          :aria-label="`阅读进度 ${scrollPercent}`"
-        >
-          {{ scrollPercent }}
-        </span>
+  <button
+    v-if="toTop"
+    class="btt-btn floating-button bottom-10"
+    title="回到顶部"
+    aria-label="回到顶部"
+    @click="scrollToTop"
+  >
+    <span class="btt-icon text-xl i-mdi-chevron-double-up" />
+    <span class="btt-percent hidden">
+      <span class="flex justify-center">
+        <span class="text-xs i-mdi-chevron-double-up" />
       </span>
-    </button>
-    <button
-      v-else
-      class="btt-btn floating-button bottom-10"
-      title="前往底部"
-      aria-label="前往底部"
-      @click="scrollToBottom"
-    >
-      <span class="btt-icon text-xl i-mdi-chevron-double-down" />
-      <span class="btt-percent hidden">
-        <span
-          class="text-sm"
-          :aria-label="`阅读进度 ${scrollPercent}`"
-        >
-          {{ scrollPercent }}
-        </span>
-        <span class="flex justify-center">
-          <span class="text-xs i-mdi-chevron-double-down" />
-        </span>
+      <span
+        class="text-sm"
+        :aria-label="`阅读进度 ${scrollPercent}`"
+      >
+        {{ scrollPercent }}
       </span>
-    </button>
-  </template>
+    </span>
+  </button>
+  <button
+    v-else
+    class="btt-btn floating-button bottom-10"
+    title="前往底部"
+    aria-label="前往底部"
+    @click="scrollToBottom"
+  >
+    <span class="btt-icon text-xl i-mdi-chevron-double-down" />
+    <span class="btt-percent hidden">
+      <span
+        class="text-sm"
+        :aria-label="`阅读进度 ${scrollPercent}`"
+      >
+        {{ scrollPercent }}
+      </span>
+      <span class="flex justify-center">
+        <span class="text-xs i-mdi-chevron-double-down" />
+      </span>
+    </span>
+  </button>
 </template>
 
 <script setup lang="ts">
@@ -54,16 +52,14 @@ import { ref } from 'vue';
 const { y: scrollY } = useWindowScroll();
 const { height: windowHeight } = useWindowSize();
 
-const display = ref(false);
 const toTop = ref(false);
 const scrollPercent = ref('');
 
 watchThrottled([windowHeight, scrollY], () => {
   if (!import.meta.env.SSR) {
-    display.value = document.body.scrollHeight >= windowHeight.value * 1.5;
-    toTop.value = scrollY.value >= windowHeight.value / 2;
     const percent = (scrollY.value / (document.body.scrollHeight - windowHeight.value)) * 100;
     scrollPercent.value = `${percent.toFixed(0)}%`;
+    toTop.value = scrollY.value >= windowHeight.value / 2 || percent > 50;
   }
 }, {
   immediate: true,
