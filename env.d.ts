a5b3ce3918cd6d68231433de3bc9f2020e6f23bb
Yufan You <ouuansteve@gmail.com>
2023-02-22 14:18:22 +0800

    refactor: use reference instead of import for ts-reset

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,4 @@
 /// <reference types="vite/client" />
 /// <reference types="iles" />
 /// <reference types="vite-plugin-info/client" />
-
-// eslint-disable-next-line import/no-extraneous-dependencies
-import '@total-typescript/ts-reset';
+/// <reference types="@total-typescript/ts-reset" />
