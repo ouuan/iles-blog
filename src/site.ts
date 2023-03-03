90fbc71e00f64838681b7f0d15c64432947e24b2
Yufan You <ouuansteve@gmail.com>
2023-03-03 11:50:00 +0800

    fix: update meta description
    草，你怎么又闲着没事改 meta description（

--- src/site.ts
+++ src/site.ts
@@ -5,7 +5,7 @@ export default {
   lastName: 'You',
   gender: 'male',
   title: "ouuan's blog",
-  description: '欢迎来到 ouuan 的博客！本博客以技术向内容为主，包括 Web、系统、Linux、大学生活等方面的学习笔记和经验分享。',
+  description: 'ouuan 写东西的地方，涵盖了 Web 开发、计算机系统、Linux 使用、大学学习生活等多个主题。',
   startedAt: new Date('2022-07-20T08:00:00Z'),
   repo: 'ouuan/iles-blog',
   branch: 'master',
