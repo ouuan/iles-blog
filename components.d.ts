30945171dadcce7d24cd89d6b526009099e03898
Yufan You <ouuansteve@gmail.com>
2023-03-22 13:17:03 +0800

    content(thuse): add 偷窥同学代码

--- components.d.ts
+++ components.d.ts
@@ -21,7 +21,7 @@ declare module '@vue/runtime-core' {
     GiscusCommentsInner: typeof import('./src/components/GiscusCommentsInner.vue')['default']
     Head: typeof import('@vueuse/head')['Head']
     HomePage: typeof import('./src/components/HomePage.vue')['default']
-    Island: typeof import('./node_modules/.pnpm/iles@0.9.2_x7dzuadbe27hczg4caescy3qve/node_modules/iles/dist/client/app/components/Island.vue')['default']
+    Island: typeof import('./node_modules/.pnpm/iles@0.9.2_2xfa3zjpjrc7d2ftmvs4djbo5a/node_modules/iles/dist/client/app/components/Island.vue')['default']
     NavBar: typeof import('./src/components/NavBar.vue')['default']
     NavBarItem: typeof import('./src/components/NavBarItem.vue')['default']
     PaginationBar: typeof import('./src/components/PaginationBar.vue')['default']
