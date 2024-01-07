ed32e14e10a816dfd7c21f07205dc773af09a755
Yufan You <ouuansteve@gmail.com>
2024-01-08 00:10:58 +0800

    chore: update components.d.ts

--- components.d.ts
+++ components.d.ts
@@ -20,7 +20,7 @@ declare module '@vue/runtime-core' {
     GiscusCommentsInner: typeof import('./src/components/GiscusCommentsInner.vue')['default']
     Head: typeof import('@vueuse/head')['Head']
     HomePage: typeof import('./src/components/HomePage.vue')['default']
-    Island: typeof import('./node_modules/.pnpm/iles@0.9.5_@types+node@18.19.3_esbuild@0.19.8_sass@1.69.5_typescript@5.3.3/node_modules/iles/dist/client/app/components/Island.vue')['default']
+    Island: typeof import('./node_modules/.pnpm/iles@0.9.5_@types+node@18.19.4_esbuild@0.19.8_sass@1.69.7_typescript@5.3.3/node_modules/iles/dist/client/app/components/Island.vue')['default']
     MeiliSearch: typeof import('./src/components/MeiliSearch.vue')['default']
     NavBar: typeof import('./src/components/NavBar.vue')['default']
     NavBarItem: typeof import('./src/components/NavBarItem.vue')['default']
